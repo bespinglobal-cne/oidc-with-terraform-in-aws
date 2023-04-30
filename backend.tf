dbe81cd3f36ed7462176e73909e8d59c7081e823
황광진 <hwang-gwangjin@hwang-gwangjins-MacBook-Pro.local>
2023-04-30 12:19:46 +0900

    update code

--- backend.tf
+++ backend.tf
@@ -3,6 +3,5 @@ terraform {
     bucket   = "kwangjin-tfstate"
     region   = "ap-northeast-2"
     key      = "terraform/output/oidc-with-terraform-in-aws"
-    role_arn = "arn:aws:iam::767404772322:role/github-actions"
   }
 }
